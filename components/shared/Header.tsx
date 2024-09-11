f35a0b8afcfc11119132a7f5cb57161936d8fb41
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2024-09-11 19:32:29 -0300

    feat: header component

---
+++ components/shared/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+interface HeaderProps {
+  title: string,
+  subtitle: string
+}
+function Header({ title, subtitle }: HeaderProps) {
+  return (
+    <>
+      <h2 className='h2-bold text-dark-600'>{title}</h2>
+      {
+        subtitle && <p className='p-16-regular mt-4'>{subtitle}</p>
+      }
+    </>
+  )
+}
+
+export default Header
