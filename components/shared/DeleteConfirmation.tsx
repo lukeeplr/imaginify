c52a835197ae577c74ce772f9fcf6a8b6aa00c16
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2024-09-14 03:19:50 -0300

    feat: delete image button/ dialog

---
+++ components/shared/DeleteConfirmation.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useTransition } from "react";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { deleteImage } from "@/lib/actions/image.actions";
+
+import { Button } from "../ui/button";
+
+export const DeleteConfirmation = ({ imageId }: { imageId: string }) => {
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild className="w-full rounded-full">
+        <Button
+          type="button"
+          className="button h-[44px] w-full md:h-[54px]"
+          variant="destructive"
+        >
+          Apagar imagem
+        </Button>
+      </AlertDialogTrigger>
+
+      <AlertDialogContent className="flex flex-col gap-10">
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Tem certeza que você quer apagar esta imagem?
+          </AlertDialogTitle>
+          <AlertDialogDescription className="p-16-regular">
+            Isso vai deletar permanentemente essa imagem.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction
+            className="border bg-red-500 text-white hover:bg-red-600"
+            onClick={() =>
+              startTransition(async () => {
+                await deleteImage(imageId);
+              })
+            }
+          >
+            {isPending ? "Deletando..." : "Deletar"}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
