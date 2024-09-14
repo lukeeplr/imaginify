d201e965040a13a9d4d06b03d88d3b5364a4e6e1
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2024-09-14 03:18:13 -0300

    feat: homepage

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -7,11 +7,13 @@ import User from "../database/models/user.model"
 import Image from "../database/models/image.model"
 import { redirect } from "next/navigation"
 
+import { v2 as cloudinary } from 'cloudinary'
+
 
 const populateUser = (query: any) => query.populate({
     path: 'author',
     model: User,
-    select: '_id firstName lastName'
+    select: '_id firstName lastName clerkId'
 })
 
 export async function addImage({ image, userId, path}: AddImageParams) {
@@ -91,6 +93,63 @@ export async function getImageById(imageId: string) {
         if (!image) throw new Error("Imagem não encontrada.")
 
         return JSON.parse(JSON.stringify(image))
+
+    } catch(error) {
+        handleError(error)
+    }
+
+}
+
+
+export async function getAllImages({limit = 9, page = 1, searchQuery = ""}: {
+    limit?: number
+    page: number
+    searchQuery?: string}) {
+
+    try {
+        await connectToDatabase()
+
+        cloudinary.config({
+            cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET,
+            secure: true
+        })
+
+        let expression = 'folder=imaginify'
+
+        if (searchQuery) {
+            expression += ` AND ${searchQuery}`
+        }
+
+        const { resources } = await cloudinary.search
+            .expression(expression)
+            .execute()
+
+        const resourceIds = resources.map((resource: any) => resource.public_id)
+
+        let query = {}
+
+        if (searchQuery) {
+            query = {
+                publicId: {
+                    $in: resourceIds
+                }
+            }
+        }
+
+        const skipAmount = (Number(page) - 1) * limit
+
+        const images = await populateUser(Image.find(query).skip(skipAmount).limit(limit).sort({updatedAt: -1}))
+
+        const totalImages = await Image.find(query).countDocuments()
+        const savedImages = await Image.find().countDocuments()
+
+        return {
+            data: JSON.parse(JSON.stringify(images)),
+            totalPages: Math.ceil(totalImages / limit),
+            savedImages
+        }
 
     } catch(error) {
         handleError(error)
